14de39d2a2dca2403b8f6dc9801c37c24f16e949
root <root@aus-navi3x-13.amd.com>
2024-04-16 20:11:43 -0500

    util adjust

--- pyt_llm_inference_DLM.py
+++ pyt_llm_inference_DLM.py
@@ -177,9 +177,8 @@ def main():
             max_num_batched_tokens = 16384
         elif args.model_path == "TheBloke/Llama-2-70B-Chat-GPTQ" or args.model_path == "TheBloke/Llama-2-70B-GPTQ":
             if args.n_gpu == 1:
-                #max_num_batched_tokens =  4096
-                max_num_batched_tokens =  3000 # hack for benchmark test
-                gpu_memory_utilization = 0.98 # workaround to prevent oom
+                max_num_batched_tokens =  4096
+                gpu_memory_utilization = 1.00 # workaround to prevent oom
             else:
                 max_num_batched_tokens =  4096
                 gpu_memory_utilization =  0.8 # workaround to prevent oom
